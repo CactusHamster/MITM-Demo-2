6d57c364778fafa010173fb2c95fe40777a1cb93
Robin <66969441+CactusHamster@users.noreply.github.com>
2023-04-26 00:13:13 -0700

    fix webpack images finally

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,11 @@ module.exports = {
             },
             {
                 test: /\.(png|jpe?g|gif)$/i,
-                use: { loader: 'file-loader', options: { outputPath: 'img/', name: '[name].[ext]' } },
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'img',
+                    name: "[name].[ext]"
+                }
             },
         ],
     },
