d48d02bb85d3ee90280564c24431d180e603e89e
Robin <66969441+CactusHamster@users.noreply.github.com>
2023-04-25 22:29:58 -0700

    login page

---
+++ src/public/login/index.ts
@@ -0,0 +1,71 @@
+import "./index.css"
+//import "./ubuntu.woff2"
+let checkInput = (elem: any): elem is HTMLInputElement => {
+    let r = elem instanceof HTMLInputElement;
+    if (r === false) throw new Error(`Element \`${elem.id}\` is not an input element.`);
+    return r;
+}
+let onEnter = (elem: HTMLElement, action: Function) => {
+    elem.addEventListener("keydown", (e) => e.code === "Enter" ? action(e) : null)
+}
+;(function () {
+    const userbox = document.getElementById("uname");
+    const passbox = document.getElementById("passwd");
+    const gobutton = document.getElementById("gobtn");
+    if (!checkInput(userbox) || !checkInput(passbox) || !checkInput(gobutton)) throw new Error("Required input is not an input element.");
+    if (gobutton === null) throw new Error("Element `gobtn` is null.");
+
+    onEnter(userbox, () => passbox.focus())
+    onEnter(passbox, () => gobutton.click())
+
+    function showError (err: string | Error) {
+        const errspan = document.getElementById("puterr");
+        if (!errspan) throw new Error("Element `puterr` is null.");
+        if (err instanceof Error) err = err.message;
+        errspan.innerText = err;
+    }
+
+    let authenticating = false;
+    async function checkLogin (): Promise<void> {
+        if (!checkInput(userbox) || !checkInput(passbox) || !checkInput(gobutton)) throw new Error("Required input is not an input element.");
+        let oldButtonText = gobutton.innerText;
+        let reset = () => {
+            authenticating = false;
+            gobutton.disabled = false;
+            gobutton.innerText = oldButtonText;
+        }
+        try {
+            // ensure no other instances of function are running
+            if (authenticating) throw new Error("");
+            let email = userbox.value;
+            let password = passbox.value;
+            if (email === "") throw new Error("Invalid email.");
+            else if (password === "") throw new Error("Invalid password.");
+            authenticating = true;
+            gobutton.disabled = true;
+            gobutton.innerText = "Authenticating...";
+            let res: Response;
+            let data: any;
+            try {
+                res = await fetch("/api/login", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        user: email,
+                        password: password
+                    })
+                });
+                data = await res.json();
+            } catch (e) { throw new Error("Failed to contact server."); }
+            reset()
+            if (!res.ok) throw new Error(`[${res.status}]: ${data.message ?? "<no response>"}`);
+            if (!data.token) throw new Error("Failed to understand remote server.");
+            localStorage.setItem("token", data.token)
+            document.location = "/app"
+        } catch (e) {
+            reset();
+            if (!(e instanceof Error) && typeof e !== "string") return console.error(e);
+            showError(e);
+        }
+    }
+    gobutton.addEventListener("click", checkLogin)
+})();
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: "development",
+    entry: './src/public/index.ts',
+    entry: {
+        app: [path.resolve(__dirname, "src", "public", "app", "index.ts")],
+        login: [path.resolve(__dirname, "src", "public", "login", "index.ts")],
+      },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist/public/',
+    },
+    target: "web",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [ { loader: 'file-loader', options: { name: '[name].[ext]', outputPath: 'fonts/' } } ]
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, 'dist', 'public'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Log in',
+            filename: 'login.html',
+            template: "./src/public/login/index.html",
+            chunks: ["login"]
+        }),
+        new HtmlWebpackPlugin({
+            title: 'MITM Proxy',
+            filename: 'app.html',
+            template: "./src/public/app/index.html",
+            chunks: ["app"]
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/public/index.html",
+            "filename": "index.html",
+            chunks: []
+        })
+    ]
+};
